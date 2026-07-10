60eb6c05e5645144ddcfafd7df829cdf54528e94
danonino25 <dananita2003mar@gmail.com>
2026-07-09 20:15:48 -0600

    feat(tv): add TvCatalogScreen with TvLazyColumn/TvLazyRow and two data rows

---
+++ tv/TVActivity.kt
@@ -0,0 +1,21 @@
+package mx.utng.smarthealthmonitor.tv
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import mx.utng.smarthealthmonitor.tv.presentation.TvCatalogScreen
+
+class TVActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            val navController = rememberNavController()
+            NavHost(navController = navController, startDestination = "catalog") {
+                composable("catalog") { TvCatalogScreen() }
+            }
+        }
+    }
+}
